7be82f28adc7e13aa609241b9fc8d014ef1c692e
tobiillz <ayoadeopeoluwa@yahoo.com>
2022-01-31 15:22:15 +0100

    added card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import '../App.css';
+import star from '../images/Star 1.png'
 
 function Card() {
-  return <div>
-      <p>Text Here</p>
+  return <div className='card'>
+      <div className='card-template'>
+         <button className='btn'>
+           SOLD OUT
+         </button>
+      </div>
+      <div className='card-stats'>
+            <img src={star} alt="star" className='card-star' />
+            <span>5</span>
+            <span className='grey'>(6) *</span>
+            <span className='grey'>USA </span>
+      </div>
+      <p>Life Lesson from Katie Zafereys</p>
+      <p> <b>From $136</b>/ person</p>
   </div>;
 }
 
