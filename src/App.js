a790ce4af32df54fd2addf2f0c385586d8c27338
tobiillz <ayoadeopeoluwa@yahoo.com>
2022-02-02 16:45:38 +0100

    last update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Card from './components/Card';
-import image12 from './images/image 12.png';
+import image12 from './images/image12.png';
 
 function App() {
   return (
@@ -12,11 +12,11 @@ function App() {
         <Hero/>
         <Card
           img = {image12}
-          rating = "5.0"
-          number = "6"
+          rating = {5.0}
+          number = {6}
           country = "USA"
           description = "Life Lesson from Katie Zafereys"
-          price = "136"        
+          price = {136}
         />
 
         {/* <Card
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import '../App.css';
 import star from '../images/Star 1.png';
-import image12 from '../images/image 12.png';
 
 function Card({img, rating , number, country, description, price}) {
+  let colors = ["red", "green", "blue"]
   return <div className='card'>
-      <img src={img} className='card-image' alt=''/>
+
+          {colors.map((color)=> <h3>{color}</h3>)}
+      
+      {/* <img src={img} className='card-image' alt=''/>
       {/* <button className='btn'>
         SOLD OUT 
       </button> */}
       <div className='card-stats'>
             <img src={star} alt="star" className='card-star' />
             <span>{rating}</span>
-            <span className='grey'>({number}) *</span>
+            <span className='grey'>({number}) * </span>
             <span className='grey'>{country} </span>
       </div>
       <p>{description}</p>
-      <p> <b>From ${price}</b>/ person</p>
+      <p> <b>From ${price}</b>/ person</p> */}
   </div>;
 }
 
